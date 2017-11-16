a790c5a2e816032a3984407408481cf943fb0ac0
douglas.silva <douglas.silva@cwi.com.br>
2017-11-16 18:52:57 -0200

    'style corrections'

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,7 +3,13 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-card',
-  template: `<div class="card-component"><ng-content></ng-content></div>`,
+  template: `
+  <div class="card-component">
+  <div class="card-component__header">
+    <h1><ng-content select="h1"></ng-content></h1>
+  </div>
+  <ng-content></ng-content>
+  </div>`,
   styleUrls:['./card.component.scss']
 })
 export class Card {
