9bb79fdd8f92e72fc7e9f9ab69d57212b990ed6c
douglas.silva <douglas.silva@cwi.com.br>
2017-11-17 15:00:14 -0200

    'new time-line component'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpModule,
   ],
--- src/app/scenes/dashboard/components/time-line/time-line.component.ts
+++ src/app/scenes/dashboard/components/time-line/time-line.component.ts
@@ -1,4 +1,4 @@
-import { Component,  EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { TimeLine } from './time-line'
 
 @Component({
@@ -11,10 +11,12 @@ export class TimeLineComponent {
 
   @Output() onAccept: EventEmitter<TimeLine> = new EventEmitter<TimeLine>();
   public timeLine: TimeLine = new TimeLine();
+  public used: boolean = false;
 
   constructor() { }
 
   public handleConfirmationButton() {
+    this.used = (this.timeLine.dtInicio && this.timeLine.dtFim) !== undefined;
     this.onAccept.emit(this.timeLine)
   }
 }
