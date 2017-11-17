aa60c44ba0e5996a2d91920f26ff683344c36241
DOUGLAS C DA SILVA <douglascdasilva@MacBook-Air-de-DOUGLAS.local>
2017-11-16 22:49:28 -0200

    social media

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -5,10 +5,14 @@ import { Component } from "@angular/core";
   selector: 'app-card',
   template: `
   <div class="card-component">
-  <div class="card-component__header">
-    <h1><ng-content select="h1"></ng-content></h1>
-  </div>
-  <ng-content></ng-content>
+	<div class="card-component__header">
+		<h1><ng-content select="h1"></ng-content></h1>
+	</div>
+	<ng-content></ng-content>
+	<div class="card-component__footer">
+		<a href="www.linkedin.com/in/douglascaina" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+		<a href="https://github.com/caina/angular4-timesheet-architecture" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>	
+	</div>
   </div>`,
   styleUrls:['./card.component.scss']
 })
